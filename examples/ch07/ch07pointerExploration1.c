2b54aa36993ad5f7ff24607a3b16f475d16e1c55
rpriceCO <111899115+rpriceCO@users.noreply.github.com>
2022-09-28 22:31:47 -0600

    comments

--- examples/ch07/ch07pointerExploration1.c
+++ examples/ch07/ch07pointerExploration1.c
@@ -9,32 +9,43 @@
 
 int main ()
 {
-	// Initalize houseNum
-	int houseNum = 13;
-	int calcHouseNum1 = 0;
-	int calcHouseNum2 = 0;
-	int *houseNumPtr = &houseNum;
-	
-	printf("houseNum %d\n", houseNum);
+    // Initalize houseNum
+    int houseNum = 13;
+    int calcHouseNum1 = 0;
+    int calcHouseNum2 = 0;
+    int *houseNumPtr = &houseNum;
 
-	printf("&houseNum %p\n\n", &houseNum);
+    // prints the variable houseNum
+    printf("houseNum %d\n", houseNum);
 
-	printf ("*houseNumPtr  %d\n", *houseNumPtr);
+    // prints the address for the variable houseNum
+    printf("&houseNum %p\n\n", &houseNum);
 
-	printf ("houseNumPtr %p\n\n", houseNumPtr);
+    // prints the variable that houseNumPtr is pointing to
+    printf ("*houseNumPtr  %d\n", *houseNumPtr);
 
-	printf ("&houseNumPtr %p\n\n", &houseNumPtr);
+    // prints the address for houseNum using the houseNumPtr
+    printf ("houseNumPtr %p\n\n", houseNumPtr);
 
-	calcHouseNum1 =  *houseNumPtr + *houseNumPtr;
+    // prints the address that is for the houseNumPtr itself
+    printf ("&houseNumPtr %p\n\n", &houseNumPtr);
 
-	printf("*houseNumPtr + *houseNumPtr is %d\n\n", calcHouseNum1);
+    // 13+13, gets the variables using the *
+    calcHouseNum1 =  *houseNumPtr + *houseNumPtr;
 
-	calcHouseNum2 = 2 * (*houseNumPtr);
+    // prints 26
+    printf("*houseNumPtr + *houseNumPtr is %d\n\n", calcHouseNum1);
 
-	printf("2 * (*houseNumPtr) is %d\n\n", calcHouseNum2);
+    // 2*13, uses the ptr to get variable again
+    calcHouseNum2 = 2 * (*houseNumPtr);
 
-	printf("\n&*houseNumPtr = %p"
-		"\n*&houseNumPtr = %p\n", &*houseNumPtr, *&houseNumPtr);
-			
-	return 0;
+    // prints 26
+    printf("2 * (*houseNumPtr) is %d\n\n", calcHouseNum2);
+
+    // the first address that is printed is the address for houseNum
+    // and the second address is the same as well
+    printf("\n&*houseNumPtr = %p"
+           "\n*&houseNumPtr = %p\n", &*houseNumPtr, *&houseNumPtr);
+
+    return 0;
 }
--- examples/ch07/ch07sec7_4CubePassRef.c
+++ examples/ch07/ch07sec7_4CubePassRef.c
@@ -1,7 +1,7 @@
 // Passing Primitive Data Types and pointers
 //Add comments to explain the code
 
-#include <stdio.h>	
+#include <stdio.h>
 #include <stdlib.h>
 
 int cubePass1(int number);
@@ -9,46 +9,58 @@ int cubePass2(int * numberPtr);
 
 int main(void) {
 
-	int numberMain = 5;
-	int result = 0;
-	
-	printf ("In main before cubePass1  numberMain = %d\n", numberMain);
-	printf("&numberMain = %p\n", &numberMain);
-	result = cubePass1(numberMain);
-	printf ("In main after cubePass1  numberMain = %d\n", numberMain);
-	printf ("Result = %d\n", result);
-	printf("\nIn main before cubePass2  numberMain = %d\n", numberMain);
-	result = cubePass2(&numberMain);
-	printf("\nIn main after cubePass2  numberMain = %d\n", numberMain);
-	printf("result = %d\n", result);
+    int numberMain = 5;
+    int result = 0;
+
+    // prints numberMain int and address
+    printf ("In main before cubePass1  numberMain = %d\n", numberMain);
+    printf("&numberMain = %p\n", &numberMain);
+
+    // result stores numberMain variable after it gets passed through function
+    result = cubePass1(numberMain);
+
+    // prints numberMain after being passed through cubePass1
+    printf ("In main after cubePass1  numberMain = %d\n", numberMain);
+    printf ("Result = %d\n", result);
+
+    // prints numberMain then passes it through cubePass2 into result
+    printf("\nIn main before cubePass2  numberMain = %d\n", numberMain);
+    result = cubePass2(&numberMain);
+
+    // prints numberMain after its been change in function cubePass2 and result, both are same
+    printf("\nIn main after cubePass2  numberMain = %d\n", numberMain);
+    printf("result = %d\n", result);
 
 } // main
 
 
-int cubePass1 (int number) 
+// function to pass by value
+int cubePass1 (int number)
 {
-	int cube = 0;
-	puts("\nIn cubePass1");
-	printf("number = %d\n", number);
-	printf("&number = %p\n", &number);
-	cube = number * number * number;
-	printf("cube  = %d\n", cube);
-	number = cube;
-	printf("number = %d\n", number);
-	return cube;
-} 
+    int cube = 0;
+    puts("\nIn cubePass1");
+    printf("number = %d\n", number);
+    printf("&number = %p\n", &number);
+    cube = number * number * number;
+    printf("cube  = %d\n", cube);
+    number = cube;
+    printf("number = %d\n", number);
+    return cube;
+}
 
-int cubePass2 (int * numberPtr) 
+// function to pass by reference
+int cubePass2 (int * numberPtr)
 {
-	int cube = 0;
-	puts ("\nIn cubePass2");
-	printf("numberPtr = %p\n", numberPtr);
-	printf ("*numberPtr = %d\n", *numberPtr);
-	printf("&numberPtr = %p\n", &numberPtr);
-	cube = (* numberPtr )* (* numberPtr )* (* numberPtr); 
-	*numberPtr = cube;
-	printf ("*numberPtr = %d\n", *numberPtr);
-	return cube;
-} 
+    int cube = 0;
+    puts ("\nIn cubePass2");
+    printf("numberPtr = %p\n", numberPtr);
+    printf ("*numberPtr = %d\n", *numberPtr);
+    printf("&numberPtr = %p\n", &numberPtr);
+    cube = (* numberPtr )* (* numberPtr )* (* numberPtr);
+    // value in numberMain get changed to cube
+    *numberPtr = cube;
+    printf ("*numberPtr = %d\n", *numberPtr);
+    return cube;
+}
 
 
